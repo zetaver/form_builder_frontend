e4b3a9f7fd605fefd7b4d7b7ff00e4cf1274d5e7
HarisAkber292 <haris@zetaver.com>
2024-11-28 11:54:55 +0500

    added frontend

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Adjust paths if necessary
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
